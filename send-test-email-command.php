42b435feb27f5e0d99e36684e43466cdfbd9cef9
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2023-08-16 17:37:15 +0200

    first commit

---
+++ send-test-email-command.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WP_CLI\SendEmail;
+
+use WP_CLI;
+
+if (!class_exists('\WP_CLI')) {
+	return;
+}
+
+$wpcli_send_test_email_autoloader = __DIR__ . '/vendor/autoload.php';
+
+if (file_exists($wpcli_send_test_email_autoloader)) {
+	require_once $wpcli_send_test_email_autoloader;
+}
+
+WP_CLI::add_command('send-email', SendTestEmailCommand::class);
---
+++ src/SendTestEmailCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace WP_CLI\SendTestEmail;
+
+use WP_CLI;
+use WP_CLI_Command;
+
+class SendTestEmailCommand extends WP_CLI_Command
+{
+
+	/**
+	 * Send a test email
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     # Send a test email.
+	 *     $ wp send-test-email
+	 *     Success: Hello World!
+	 *
+	 * @when before_wp_load
+	 *
+	 * @param array $args       Indexed array of positional arguments.
+	 * @param array $assoc_args Associative array of associative arguments.
+	 */
+	public function __invoke($args, $assoc_args)
+	{
+		wp_mail(get_bloginfo('admin_email'), 'This is a test email !', 'Sent from ' . get_bloginfo('site_url'), ['Content-Type: text/html; charset=UTF-8']);
+		WP_CLI::success('Hello World!');
+	}
+}
